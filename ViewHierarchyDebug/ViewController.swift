b4c660ba52d986859d639ebd4f4927594d50e162
sierra OS <sierra@sierraui-macbook-air.local>
2019-01-26 23:34:45 +0900

    Initial Commit

---
+++ ViewHierarchyDebug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewHierarchyDebug
+//
+//  Created by sierra OS on 2019. 1. 26..
+//  Copyright © 2019년 daehoon ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
