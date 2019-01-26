f3a32570d41934fd6d702d85037d0441856285e7
sierra OS <sierra@sierraui-macbook-air.local>
2019-01-27 01:03:44 +0900

    sum property add!!

--- ViewHierarchyDebug/ViewController.swift
+++ ViewHierarchyDebug/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var sum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
